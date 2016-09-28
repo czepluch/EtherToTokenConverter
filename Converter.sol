139be673fd95d3403f4eda2d8ca494a2c8f670fc
Jacob Stenum Czepluch <j.czepluch@gmail.com>
2016-09-28 09:54:23 +0200

    added some documentation

--- Converter.sol
+++ Converter.sol
@@ -15,6 +15,11 @@ contract Converter is StandardToken {
         _//;
     }
 
+    /// @notice Contructor for the token.
+    /// @dev Constructor that takes different arguments to determine specifications of the token
+    /// @param _name (string) name of the token
+    /// @param _symbol (string) symbol of the token
+    /// @param _version (string) version of the token
     function Converter(string _name, string _symbol, string _version) {
         name = _name;
         decimals = 18;
@@ -22,6 +27,8 @@ contract Converter is StandardToken {
         version = _version;
     }
 
+    /// @notice function for receiving ether value
+    /// @dev function that receives ether and issues tokens corresponding to the amount of ether
     //function() payable {
     function() {
         uint amount = msg.value;
@@ -30,18 +37,29 @@ contract Converter is StandardToken {
         TokensCreated(msg.sender, amount);
     }
 
+    /// @notice Get the balance of the contract in ether
+    /// @dev Get the balance of the contract in ether
+    /// @return balance (uint) the balance of the contract in ether
     function getEtherBalance() constant returns (uint balance) {
         balance = this.balance * 1 ether;
     }
 
-    function getTokenBalance() constant returns (uint balance) {
-        balance = balances[this] * 1 ether;
+    /// @notice Get the total amount of tokens issued
+    /// @dev Total amount of tokens issued
+    /// @return balance (uint) the amount of tokens issued
+    function getTokenIssued() constant returns (uint balance) {
+        balance = totalSupply * 1 ether;
     }
 
+    /// @notice Redeem tokens for ether
+    /// @dev Redeem tokens for ether
+    /// @param amount (uint) the amount of tokens to redeem
+    /// @return (bool) to show that transfer was successful
     function convertTokenToEther(uint amount) hasBalance(amount) returns (bool) {
         balances[msg.sender] -= amount;
         if (!msg.sender.send(amount)) throw;
         totalSupply -= amount;
         EtherRedeemed(msg.sender, amount);
+        return true;
     }
 }
